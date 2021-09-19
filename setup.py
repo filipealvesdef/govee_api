6b1dd31baf2461c9e9c2ec9776da41afe8235f7d
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-18 22:47:49 -0300

    setup: update version 1.3.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/filipealvesdef/govee_api/tree/filipealves-setup-fixes',
-    version='1.2.3',
+    version='1.3.0',
     zip_safe=False,
 )
